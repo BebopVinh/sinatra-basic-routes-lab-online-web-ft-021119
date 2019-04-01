22df87ba8c071c8012f99e5ff997946ffc1a319a
Vinh Do <vinhdo07@gmail.com>
2019-04-01 00:54:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is Vinh"
+    "My name is Vinh."
+  end
+
+  get '/hometown' do
+    "My hometown is Long Beach."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is always on the radio."
   end
 end
